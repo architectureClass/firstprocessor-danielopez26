8de72d9bf008901298b2ad91c520de6dee6e731f
danielopez26 <danielopezmurcia@gmail.com>
2017-04-20 14:25:28 -0500

    FirstProcessor Daniel Lopez - David Beltran

---
+++ CU.vhd
@@ -0,0 +1,44 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity CU is
+    Port ( Op : in  STD_LOGIC_VECTOR (1 downto 0);
+           Op3 : in  STD_LOGIC_VECTOR (5 downto 0);
+           CU_out : out  STD_LOGIC_VECTOR (3 downto 0));
+end CU;
+
+architecture Behavioral of CU is
+
+begin
+
+	process (Op, Op3)	
+	begin
+	-- op siempre sera 10 
+	-- op3 siempre lo dan cuaderno 
+		if(op = "10") then
+			case (Op3) is
+				when("000000") =>
+				--indica a la unidad de control que operacion se hace 
+					CU_out <= "0001"; -- Add
+				when("000100") =>
+					CU_out <= "0010"; -- Sub
+				when("000001") =>
+					CU_out <= "0011"; -- And
+				when("000101") =>
+					CU_out <= "0100"; -- Nand
+				when("000010") =>
+					CU_out <= "0101"; -- Or	
+				when("000110") =>
+					CU_out <= "0110"; -- Nor
+				when("000011") =>
+					CU_out <= "0111"; -- Xor
+				when("000111") =>
+					CU_out <= "1000"; -- Xnor				
+				when others =>
+					CU_out <= "0000";
+			end case;
+		end if;
+	end process;
+
+end Behavioral;
+
--- ProgramCounter.vhd
+++ ProgramCounter.vhd
@@ -1,26 +1,30 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+
 entity ProgramCounter is
     Port ( clk : in  STD_LOGIC;
-           pc_entrada : in  STD_LOGIC_VECTOR (31 downto 0);
-           pc_salida : out  STD_LOGIC_VECTOR (31 downto 0);
-           reset : in  STD_LOGIC);
+           rst : in  STD_LOGIC;
+           pc_int : in  STD_LOGIC_VECTOR (31 downto 0);
+           pc_out : out  STD_LOGIC_VECTOR (31 downto 0));
 end ProgramCounter;
 
 architecture Behavioral of ProgramCounter is
 
 begin
-
-process (clk, reset, pc_entrada)
+--dice cual es la instruccion actual 
+process (clk, rst, pc_int)
 	begin
-		if (reset = '1') then
-			pc_salida <= (others => '0');
+		if (rst = '1') then
+			pc_out <= (others => '0');
 		else
 			if(rising_edge(clk)) then
-				pc_salida <= pc_entrada;
+				pc_out <= pc_int;
 			end if;
 		end if;
 end process;
 
 end Behavioral;
+
+
+
